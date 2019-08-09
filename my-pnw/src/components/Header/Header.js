8dbf57465fdd12f9769d102e12f6134d527d3d85
darms <darmcca@gmail.com>
2019-08-08 18:54:41 -0700

    adding in basic styling for components

---
+++ my-pnw/src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const element =
+<div>
+    <h1 id="slim">PNW</h1><h1 class="fat">Network</h1><br></br>
+    <h1 class="fat">Solutions</h1>
+</div>
+
+
+
+export default Header;
